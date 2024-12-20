5870feb7afab312a869b4ebd0848d7e309b5985d
DeadliestOne <uncountableaura@gmail.com>
2024-12-20 15:55:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,8 +49,8 @@ AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 # Get this credentials from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
-
-
+ 
+filter_users = 6806897901
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 

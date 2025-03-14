10566857c911bbae0262b7ec4041f173cc14d575
RiyoGod <godsxrio@gmail.com>
2025-03-15 01:29:13 +0530

    Update ping.py

--- AnonXMusic/plugins/tools/ping.py
+++ AnonXMusic/plugins/tools/ping.py
@@ -1,3 +1,4 @@
+import random
 from datetime import datetime
 
 from pyrogram import filters
@@ -8,20 +9,33 @@ from AnonXMusic.core.call import Anony
 from AnonXMusic.utils import bot_sys_stats
 from AnonXMusic.utils.decorators.language import language
 from AnonXMusic.utils.inline import supp_markup
-from config import BANNED_USERS, PING_IMG_URL
+from config import BANNED_USERS
 
+# Define 4 Ping Images
+PING_IMAGES = [
+    "https://files.catbox.moe/rpzdyk.jpg",
+    "https://files.catbox.moe/u0ljod.jpg",
+    "https://files.catbox.moe/6g1avn.jpg",
+    "https://files.catbox.moe/sudvc2.jpg",
+]
 
-@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["ping"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     start = datetime.now()
+
+    # Select a random ping image
+    ping_image = random.choice(PING_IMAGES)
+
     response = await message.reply_photo(
-        photo=PING_IMG_URL,
+        photo=ping_image,
         caption=_["ping_1"].format(app.mention),
     )
+
     pytgping = await Anony.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
+
     await response.edit_text(
         _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
         reply_markup=supp_markup(_),

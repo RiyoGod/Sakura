baf2f0a774251b76f60bdef8d03acacca328be3b
RiyoGod <godsxrio@gmail.com>
2025-03-16 16:41:20 +0530

    Update couples.py

--- AnonXMusic/plugins/tools/couples.py
+++ AnonXMusic/plugins/tools/couples.py
@@ -1,48 +1,58 @@
-import random
-from pyrogram import Client, filters
+/eval import random
+from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from AnonXMusic import app  # Use your bot instance
 
 COUPLE_QUOTES = [
-    "💓 **ʟᴏᴠᴇ ɪꜱɴ'ᴛ ᴀʙᴏᴜᴛ ᴘᴇʀꜰᴇᴄᴛɪᴏɴ, ɪᴛ'ꜱ ᴀʙᴏᴜᴛ ꜰɪɴᴅɪɴɢ ꜱᴏᴍᴇᴏɴᴇ ᴡʜᴏ ᴍᴀᴋᴇꜱ ʏᴏᴜ ᴘᴇʀꜰᴇᴄᴛʟʏ ᴄᴏᴍᴘʟᴇᴛᴇ.** 🦋",
-    "🦋 **ʟᴏᴠᴇ ɪꜱ ᴀ ᴊᴏᴜʀɴᴇʏ ᴡɪᴛʜ ᴛʜᴇ ʀɪɢʜᴛ ᴘᴇʀꜱᴏɴ ʙʏ ʏᴏᴜʀ ꜱɪᴅᴇ.** 💓",
-    "💓 **ᴡʜᴇɴ ʜᴇᴀʀᴛꜱ ʙᴇᴀᴛ ᴀꜱ ᴏɴᴇ, ᴅᴇꜱᴛɪɴʏ ᴘʟᴀʏꜱ ɪᴛꜱ ᴘᴀʀᴛ.** 🦋",
-    "🦋 **ɪɴ ᴛʜᴇ ʙᴏᴏᴋ ᴏꜰ ʟɪꜰᴇ, ʏᴏᴜ ᴀʀᴇ ᴛʜᴇ ᴄʜᴀᴘᴛᴇʀ ɪ ɴᴇᴠᴇʀ ᴡᴀɴᴛ ᴛᴏ ᴇɴᴅ.** 💓"
+    "💓 <b>ʟᴏᴠᴇ ɪsɴ'ᴛ ғᴏᴜɴᴅ, ɪᴛ's ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴛᴡᴏ ʜᴇᴀʀᴛs ᴛʜᴀᴛ ʙᴇᴀᴛ ᴀs ᴏɴᴇ.</b>",
+    "🦋 <b>ᴛʜᴇ ʙᴇsᴛ ʟᴏᴠᴇ ᴡᴀs ɴᴇᴠᴇʀ ᴘʟᴀɴɴᴇᴅ, ɪᴛ ʜᴀᴘᴘᴇɴᴇᴅ ᴡɪᴛʜ ᴛʜᴇ ʀɪɢʜᴛ ᴘᴇʀsᴏɴ.</b>",
+    "💓 <b>ʏᴏᴜ ᴅᴏɴ'ᴛ ɴᴇᴇᴅ ᴛᴏ ғɪɴᴅ ʟᴏᴠᴇ, ʟᴏᴠᴇ ᴡɪʟʟ ғɪɴᴅ ʏᴏᴜ.</b>",
+    "🦋 <b>ʟᴏᴠᴇ ɪsɴ'ᴛ ᴀ ᴅᴇsᴛɪɴᴀᴛɪᴏɴ, ɪᴛ's ᴀ ᴊᴏᴜʀɴᴇʏ ᴛʜᴀᴛ sᴛᴀʀᴛs ᴡɪᴛʜ ᴛʜᴇ ʀɪɢʜᴛ ᴘᴇʀsᴏɴ.</b>",
+    "💓 <b>ʀᴇᴀʟ ʟᴏᴠᴇ ʜᴀᴘᴘᴇɴs ᴡʜᴇɴ ᴛᴡᴏ sᴏᴜʟs ᴄᴏɴɴᴇᴄᴛ, ɴᴏᴛ ᴊᴜsᴛ ʜᴇᴀʀᴛs.</b>",
+    "🦋 <b>ᴍᴀʏʙᴇ ʏᴏᴜ'ʀᴇ ᴛʜᴇ ᴍɪʀᴀᴄʟᴇ ᴛʜᴀᴛ sᴏᴍᴇᴏɴᴇ ʜᴀs ʙᴇᴇɴ ᴡᴀɪᴛɪɴɢ ғᴏʀ.</b>",
+    "💓 <b>ʟᴏᴠᴇ ᴅᴏᴇsɴ'ᴛ ɴᴇᴇᴅ ᴛᴏ ʙᴇ ᴘᴇʀғᴇᴄᴛ, ɪᴛ ᴊᴜsᴛ ɴᴇᴇᴅs ᴛᴏ ʙᴇ ᴛʀᴜᴇ.</b>",
+    "🦋 <b>ʜᴇᴀʀᴛs ᴛʜᴀᴛ ʀᴇsᴏɴᴀᴛᴇ ᴄᴀɴ ɴᴇᴠᴇʀ ʙᴇ ᴘᴜʟʟᴇᴅ ᴀᴘᴀʀᴛ.</b>",
+    "💓 <b>ʟᴏᴠᴇ ɪsɴ'ᴛ ᴀʙᴏᴜᴛ ғɪɴᴅɪɴɢ ᴛʜᴇ ᴘᴇʀғᴇᴄᴛ ᴘᴇʀsᴏɴ, ɪᴛ's ᴀʙᴏᴜᴛ sᴇᴇɪɴɢ ᴀɴ ɪᴍᴘᴇʀғᴇᴄᴛ ᴘᴇʀsᴏɴ ᴘᴇʀғᴇᴄᴛʟʏ.</b>",
+    "🦋 <b>ᴛʜᴇ ᴏɴᴇ ᴡʜᴏ ᴛʀᴜʟʏ ʟᴏᴠᴇs ʏᴏᴜ ᴡɪʟʟ ɴᴇᴠᴇʀ ʟᴇᴛ ɢᴏ, ɴᴏ ᴍᴀᴛᴛᴇʀ ʜᴏᴡ ᴛᴏᴜɢʜ ᴛɪᴍᴇs ɢᴇᴛ.</b>",
+    "💓 <b>ʟᴏᴠᴇ ɪs ᴀ ᴘʀᴏᴍɪsᴇ ᴏғ ᴇᴛᴇʀɴɪᴛʏ, ɴᴏᴛ ᴊᴜsᴛ ᴀ ᴍᴏᴍᴇɴᴛ ɪɴ ᴛɪᴍᴇ.</b>",
+    "🦋 <b>ɪɴ ᴛʜᴇ ᴇɴᴅ, ᴡᴇ ᴏɴʟʏ ʀᴇɢʀᴇᴛ ᴛʜᴇ ʟᴏᴠᴇ ᴡᴇ ᴡᴇʀᴇ ᴛᴏᴏ sᴄᴀʀᴇᴅ ᴛᴏ ɢɪᴠᴇ.</b>",
+    "💓 <b>ᴛʀᴜᴇ ʟᴏᴠᴇ ʜᴀs ɴᴏ ᴅᴇsɪɢɴ, ɪᴛ ᴊᴜsᴛ ʜᴀᴘᴘᴇɴs.</b>",
+    "🦋 <b>ᴇᴠᴇʀʏ ʟᴏᴠᴇ sᴛᴏʀʏ ɪs ʙᴇᴀᴜᴛɪғᴜʟ, ʙᴜᴛ ᴏᴜʀs ɪs ᴍʏ ғᴀᴠᴏʀɪᴛᴇ.</b>",
+    "💓 <b>ʟᴏᴠᴇ ɪs ᴀ ʟᴀɴɢᴜᴀɢᴇ ᴏғ ᴛʜᴇ ʜᴇᴀʀᴛ, ɴᴏᴛ ᴏғ ᴛʜᴇ ᴍɪɴᴅ.</b>",
+    "🦋 <b>ᴏɴᴄᴇ ɪɴ ᴀ ʟɪғᴇᴛɪᴍᴇ, ʏᴏᴜ ғɪɴᴅ sᴏᴍᴇᴏɴᴇ ᴡʜᴏ ᴄʜᴀɴɢᴇs ᴇᴠᴇʀʏᴛʜɪɴɢ.</b>",
+    "💓 <b>ᴛʜᴇ ʀɪɢʜᴛ ᴘᴇʀsᴏɴ ᴅᴏᴇsɴ'ᴛ ᴄᴏᴍᴇ ʙʏ ᴄʜᴀɴᴄᴇ, ᴛʜᴇʏ ᴀʀᴇ ᴀ ʙʟᴇssɪɴɢ.</b>",
+    "🦋 <b>ᴛʜᴇ ʙᴇᴀᴜᴛʏ ᴏғ ʟᴏᴠᴇ ɪs ɴᴏᴛ ɪɴ ᴛʜᴇ ᴘᴇʀғᴇᴄᴛɪᴏɴ, ʙᴜᴛ ɪɴ ᴛʜᴇ ᴀᴄᴄᴇᴘᴛᴀɴᴄᴇ.</b>",
+    "💓 <b>ʟᴏᴠᴇ ɪs ᴛʜᴇ ᴏɴʟʏ ᴛʜɪɴɢ ᴛʜᴀᴛ ɢʀᴏᴡs ᴡʜᴇɴ ɪᴛ's sʜᴀʀᴇᴅ.</b>",
+    "🦋 <b>ɴᴏ ᴍᴀᴛᴛᴇʀ ᴡʜᴇʀᴇ ʟɪғᴇ ᴛᴀᴋᴇs ᴜs, ʀᴇᴀʟ ʟᴏᴠᴇ ᴀʟᴡᴀʏs ғɪɴᴅs ᴀ ᴡᴀʏ.</b>",
 ]
 
-@Client.on_message(filters.command("couples") & filters.group)
+@app.on_message(filters.command("couples") & filters.group)
 async def random_couple(client, message):
     chat_id = message.chat.id
-    members = [u for u in await client.get_chat_members(chat_id) if not u.user.is_bot]
+    members = []
+
+    async for member in client.get_chat_members(chat_id):
+        if not member.user.is_bot:  # Exclude bots
+            members.append(member.user)
 
     if len(members) < 2:
-        await message.reply("💓 **ɴᴏᴛ ᴇɴᴏᴜɢʜ ᴍᴇᴍʙᴇʀꜱ ᴛᴏ ꜰᴏʀᴍ ᴀ ᴄᴏᴜᴘʟᴇ!** 🦋")
+        await message.reply_text("Not enough members to form a couple.")
         return
 
-    user1, user2 = random.sample(members, 2)
+    user1, user2 = random.sample(members, 2)  # Select two random users
+    quote = random.choice(COUPLE_QUOTES)  # Select random quote
 
-    caption = f"""
-**➤ "ʀᴀɴᴅᴏᴍ ᴄᴏᴜᴘʟᴇ ᴏꜰ ᴛʜᴇ ᴅᴀʏ"**  
+    couple_text = f"""<b>➠ ʀᴀɴᴅᴏᴍ ᴄᴏᴜᴘʟᴇ ᴏꜰ ᴛʜᴇ ᴅᴀʏ 💓</b>
 
-💓 **ᴅᴇꜱᴛɪɴʏ ʜᴀꜱ ᴄʜᴏꜱᴇɴ:**  
-➛ **[{user1.user.first_name}](tg://user?id={user1.user.id})**  
-➛ **[{user2.user.first_name}](tg://user?id={user2.user.id})**  
+ <b>💓 ᴅᴇsᴛɪɴʏ ʜᴀs ᴄʜᴏsᴇɴ: </b>
+➛ {user1.mention}  
+➛ {user2.mention}  
 
-🦋 **ʟᴏᴠᴇ ꜰɪɴᴅꜱ ɪᴛꜱ ᴡᴀʏ ᴛᴏ ᴛʜᴏꜱᴇ ᴡʜᴏ ʙᴇʟɪᴇᴠᴇ.**  
-
-{random.choice(COUPLE_QUOTES)}
-    """
+ {quote}"""
 
     buttons = [
-        [
-            InlineKeyboardButton(
-                text=f"💓 {user1.user.first_name}",
-                url=f"tg://user?id={user1.user.id}"
-            ),
-            InlineKeyboardButton(
-                text=f"🦋 {user2.user.first_name}",
-                url=f"tg://user?id={user2.user.id}"
-            )
-        ]
+        [InlineKeyboardButton(user1.first_name, url=f"tg://openmessage?user_id={user1.id}")],
+        [InlineKeyboardButton(user2.first_name, url=f"tg://openmessage?user_id={user2.id}")],
     ]
 
-    await message.reply_text(caption, reply_markup=InlineKeyboardMarkup(buttons))
+    await message.reply_text(couple_text, reply_markup=InlineKeyboardMarkup(buttons))
